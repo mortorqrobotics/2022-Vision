ba76473c3c9bbdb21a9003d6cacdf8a011700021
Hayley <hayley.chechel@gmail.com>
2022-02-19 16:05:35 -0800

    fixed resizing issues

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 from bluePipeline import bluePipeline
 from redPipeline import redPipeline
-from redPipeline2 import redPipeline2
+from redPipeline3 import redPipeline3
 import cv2
 
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_EXPOSURE,-3.75)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_EXPOSURE,-5)
 blue = bluePipeline(cap)
-red = redPipeline2()
+red = redPipeline3()
 while True:
     # printing distance from pipeline code
     ret, frame = cap.read()
@@ -14,7 +14,7 @@ while True:
     cv2.imshow('frame', red.cv_dilate_output)
     img = frame.copy()
     cv2.drawContours(image=img, contours=red.filter_contours_output, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-    cv2.imshow('frame1', img)
+    cv2.imshow('frame1', red.resize_image_output)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
