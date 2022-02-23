1894b97dff100a5a812999e0862404f8e5279878
Hayley Chechel <hayley.chechel@gmail.com>
2022-02-23 01:54:22 +0000

    finalized pipelines

--- index.py
+++ index.py
@@ -1,25 +1,29 @@
 from bluePipeline import bluePipeline
 from redPipeline import redPipeline
 import cv2
-#bottom of camera 15 incehs above ground, and angle of aroung 70 to 85 degrees
+#bottom of camera 15 'incehs' above ground, and angle of aroung 70 to 85 degrees
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_EXPOSURE,-4)
 blue = bluePipeline()
-red = bluePipeline()
+red = redPipeline()
+
+#select pipeline
+pipeline = red
+
 def findDist(area):
     dis = 615000*(1/(area+2000))+12.2
     meters = round((0.0254*dis), 3)
     return meters
 def offset(cx,width):
-    return width/2-cx
+    return (width/2)-cx
 while True:
     # printing distance from pipeline code
     ret, frame = cap.read()
-    red.process(frame)
-    cv2.imshow('frame', red.cv_dilate_output)
-    img = frame.copy()
-    cv2.imshow('frame1', img)
-    contours = red.filter_contours_output
+    pipeline.process(frame)
+    cv2.imshow('frame', pipeline.cv_dilate_output)
+    #img = frame.copy()
+    #cv2.imshow('frame1', img)
+    contours = pipeline.filter_contours_output
     maxArea = 0
     maxIndex = 0
     width = 620
@@ -30,8 +34,9 @@ while True:
                 maxIndex = i
         contour = contours[maxIndex]
         (cx, cy), radius = cv2.minEnclosingCircle(contour)
-        #print(offset(cx, width))
-        print(findDist(cv2.contourArea(contour)))
+        off = offset(cx, width)
+        dist = findDist(cv2.contourArea(contour))
+        print(f"The ball is {dist} meters away \n with an offset of {off} pixels \n on a frame {width} pixels wide!\n")
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
