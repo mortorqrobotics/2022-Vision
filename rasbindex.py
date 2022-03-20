cf2ce39b289d08fdf249c579529a51648f74a5a2
Cameron <meme34903@gmail.com>
2022-03-20 13:35:54 -0700

    out of auto camera

--- rasbindex.py
+++ rasbindex.py
@@ -2,11 +2,15 @@ from bluePipeline import bluePipeline
 from redPipeline import redPipeline
 from networktables import NetworkTables
 import cv2
+import numpy as np
+from flask import Flask, render_template, Response, request
 
 #bottom of camera 15 inches above ground, and angle of aroung 70 to 85 degrees
 
 NetworkTables.initialize(server='10.15.15.2')
 table = NetworkTables.getTable('FRCMap')
+
+app = Flask(__name__)
 
 cap = cv2.VideoCapture(0)
 
@@ -43,8 +47,34 @@ def sendToTables(pipeline):
     table.putNumber("Offset: ", off)
     table.putNumber("Distance: ", distance)
 
+def genFrame(black=None):
+    while True:
+        _, frame = cap.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    if not feedEnabled:
+        black = np.zeros((512, 512, 3), dtype="uint8")
+        return Response(genFrame(black),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(genFrame(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def sendToCam():
+    global feedEnabled
+    feedEnabled = False
+
 while True:
-    if table.getNumber("Alliance: ", 0) == 1:
-        sendToTables(red)
-    elif table.getBoolean("Alliance: ", 0) == 2:
-        sendToTables(blue)
+    if table.getBoolean("isAutonomous", True):
+        if table.getNumber("Alliance: ", 0) == 1:
+            sendToTables(red)
+        elif table.getBoolean("Alliance: ", 0) == 2:
+            sendToTables(blue)
+    else:
+        sendToCam()
+        break
+
+app.run(host='127.0.0.1', debug=False)
